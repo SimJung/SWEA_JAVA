259625f7c3fa585cd86bb427e358b4b56a349e3f
Jung Sim <tlawjd99gma@gmail.com>
2020-07-22 16:11:03 +0900

    p10200 solved

---
+++ src/swea/p10200.java
@@ -0,0 +1,20 @@
+package swea;
+
+import java.util.Scanner;
+
+public class p10200 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tc, n, a, b;
+		Scanner sc = new Scanner(System.in);
+		tc = sc.nextInt();
+		for(int i=1; i<=tc; i++) {
+			n = sc.nextInt();
+			a = sc.nextInt();
+			b = sc.nextInt();
+			
+			System.out.println("#" + i + " "+(a > b ? b : a) + " " + (a+b-n > 0 ? a+b-n : 0));
+		}
+	}
+}
