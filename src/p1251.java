6ead293ccc6127e5b95ebabc31b34d90b4e2cd53
Jung Sim <tlawjd99gma@gmail.com>
2020-09-03 16:28:03 +0900

    p1251 kruskal, prim algorithm solved

--- src/p1251.java
+++ src/p1251.java
@@ -2,10 +2,84 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class p1251 {
+	static class Edge implements Comparable<Edge>{
+		public int to;
+		public double dist;
+		public Edge(int to, double dist) {
+			this.to = to;
+			this.dist = dist;
+		}
+		public int compareTo(Edge e) {
+			if(dist>e.dist) return 1;
+			if(dist==e.dist) return 0;
+			else return -1;
+		}
+	}
+	static int N;
+	static int[][] arr;
+	static boolean[] visit;
+	static double E, ans;
+	static ArrayList<ArrayList<Edge>> edges = new ArrayList<>();
+	static PriorityQueue<Edge> pq = new PriorityQueue<>();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc<=T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			arr = new int[N][2];
+			visit = new boolean[N];
+			ans = 0;
+			edges.clear();
+			pq.clear();
+			for(int j=0; j<2; j++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for(int i=0; i<N; i++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+					if(j == 0) edges.add(new ArrayList<>());
+				}
+			}
+			E = Double.parseDouble(br.readLine());
+			
+			for(int i=0; i<N-1; i++) {
+				for(int j=i+1; j<N; j++) {
+					edges.get(i).add(new Edge(j, getDist(i, j)));
+					edges.get(j).add(new Edge(i, getDist(i, j)));
+				}
+			}
+			
+			int cnt = 1;
+			visit[0] = true;
+			pq.addAll(edges.get(0));
+			
+			while(!pq.isEmpty()) {
+				Edge temp = pq.poll();
+				if(visit[temp.to]) continue;
+				visit[temp.to] = true;
+				cnt++;
+				ans += E * Math.pow(temp.dist, 2);
+				if(cnt == N) break;
+				
+				for(int i=0; i<edges.get(temp.to).size(); i++) {
+					if(!visit[edges.get(temp.to).get(i).to]) {
+						pq.offer(edges.get(temp.to).get(i));
+					}
+				}
+			}
+			
+			
+			System.out.println("#"+tc+" "+(long)Math.round(ans));
+		}
+
+	}
+	
+	public static double getDist(int a, int b) {
+		return Math.sqrt(Math.pow(arr[a][0] - arr[b][0], 2) + Math.pow(arr[a][1] - arr[b][1], 2));
+	}
+	/*
 	static class Edge implements Comparable<Edge>{
 		public int from;
 		public int to;
@@ -88,4 +162,5 @@ public class p1251 {
 	public static double getDist(int a, int b) {
 		return Math.sqrt(Math.pow(arr[a][0] - arr[b][0], 2) + Math.pow(arr[a][1] - arr[b][1], 2));
 	}
+	*/
 }
