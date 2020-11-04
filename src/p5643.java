5d2c27a82de00677660553d10ac1cc2760ee4182
Jung Sim <tlawjd99gma@gmail.com>
2020-11-04 23:38:41 +0900

    p5643 solved

--- src/p5643.java
+++ src/p5643.java
@@ -6,9 +6,8 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class p5643 {
-	public static final int MAX = 10000000;
 	public static int N, M, nowVal;
-	public static int dist[][];
+	public static boolean dist[][];
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
@@ -16,28 +15,21 @@ public class p5643 {
 			N = Integer.parseInt(br.readLine());
 			M = Integer.parseInt(br.readLine());
 			
-			dist = new int[N+1][N+1];
-			
-			for(int i=1; i<=N; i++) {
-				dist[i] = new int[N+1];
-				Arrays.fill(dist[i], MAX);
-				dist[i][i] = 0;
-			}
+			dist = new boolean[N+1][N+1];
 			
 			for(int i=0; i<M; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
 				
-				dist[from][to] = 1;
-				dist[to][from] = 1;
+				dist[from][to] = true;
 			}
 			
 			for(int i=1; i<=N; i++) {
 				for(int j=1; j<=N; j++) {
 					for(int k=1; k<=N; k++) {
-						if(dist[j][k] > dist[j][i] + dist[i][k]) {
-							dist[j][k] = dist[j][i] + dist[i][k];
+						if(dist[j][i]&& dist[i][k]) {
+							dist[j][k] = true;
 						}
 					}
 				}
@@ -48,7 +40,7 @@ public class p5643 {
 				boolean isOk = true;
 				for(int j=1; j<=N ;j++) {
 					if(i == j) continue;
-					if(dist[i][j] == MAX) {
+					if(!dist[i][j] && !dist[j][i]) {
 						isOk = false;
 						break;
 					}
