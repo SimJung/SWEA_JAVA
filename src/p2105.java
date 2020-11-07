a174f854753623380da6be1624566e8285074b85
Jung Sim <tlawjd99gma@gmail.com>
2020-11-07 19:58:07 +0900

    p2105 solved

--- src/p2105.java
+++ src/p2105.java
@@ -5,15 +5,9 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class p2105 {
-	public static int N;
-	public static boolean visit[] = new boolean[101];
+	public static int N, answer;
 	public static int arr[][];
-	public static int delta1[][] = {
-			{1, 1},
-			{1, -1},
-			{-1, -1},
-			{-1, 1}
-	};
+	public static boolean visit[] = new boolean[101];
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
@@ -21,7 +15,7 @@ public class p2105 {
 		for(int tc=1; tc<=T; tc++) {
 			N = Integer.parseInt(br.readLine());
 			arr = new int[N][N];
-			Arrays.fill(visit, false);
+			answer = -1;
 			
 			for(int i=0; i<N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
@@ -30,41 +24,48 @@ public class p2105 {
 				}
 			}
 			
-			int answer = 0;
-			
 			for(int i=0; i<N-2; i++) {
-				for(int j=0; j<N; j++) {
-					Arrays.fill(visit, false);
-					boolean chk = true;
-					int ans = 0;
-					for(int d=0; d<4; d++) {
-						int ni = i + delta1[d][0];
-						int nj = j + delta1[d][1];
-					
-						if(ni < 0 || ni >= N || nj < 0 || nj >= 0 || visit[arr[ni][nj]]) {
-							chk = false;
-							break;
-						}
-						visit[arr[ni][nj]] = true;
-						ans++;
-					}
-					
-					if(chk) {
-						answer = ans;
-						while(true) {
-							int ni = i + delta1[d][0];
-							int nj = j + delta1[d][1];
-						
-							if(ni < 0 || ni >= N || nj < 0 || nj >= 0 || visit[arr[ni][nj]]) {
-								chk = false;
-								break;
-							}
-						}
-					}
+				for(int j=1; j<N-1; j++) {
+					visit[arr[i][j]] = true;
+					dfs(i+1, j+1, 0, 1, 0, 1);
+					visit[arr[i][j]] = false;
 				}
 			}
 			
+			System.out.println("#"+tc+" "+answer);
 		}
+	}
+	
+	
+	public static void dfs(int i, int j, int d, int rd, int ld, int cnt) {
+		if(d == 3 && ld == 0) {
+			answer = (answer < cnt ? cnt : answer);
+			return;
+		}
+		if(i < 0 || i >= N || j < 0 || j >= N || visit[arr[i][j]]) return;
+		
+		visit[arr[i][j]] = true;
+		switch(d) {
+		case 0:
+			dfs(i+1, j-1, d+1, rd, ld+1, cnt+1);
+			dfs(i+1, j+1, d, rd+1, ld, cnt+1);
+			break;
+			
+		case 1:
+			dfs(i-1, j-1, d+1, rd-1, ld, cnt+1);
+			dfs(i+1, j-1, d, rd, ld+1, cnt+1);
+			break;
+			
+		case 2:
+			if(rd == 0) dfs(i-1, j+1, d+1, rd, ld-1, cnt+1);
+			else dfs(i-1, j-1, d, rd-1, ld, cnt+1);
+			break;
+			
+		case 3:
+			dfs(i-1, j+1, d, rd, ld-1, cnt+1);
+			break;
+		}
+		visit[arr[i][j]] = false;
 	}
 
 }
